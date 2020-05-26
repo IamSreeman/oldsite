d283b3502a5c634fec96334decccdff88fb7b26b
iamsreeman <sreemanmohanreddy@gmail.com>
2020-05-26 23:07:15 +0530

    Some updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, options);
+});
+
 $(document).ready(function() {
   Barba.Pjax.start();
 
   initializeScripts();
+
+  $(".dropdown-trigger").dropdown();
+  $('.sidenav').sidenav();
 });
